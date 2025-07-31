ea84ec7c9dc080bd3ae5b3fafe11fc48ce176b8b
David <chuksdavid2124@gmail.com>
2025-07-31 15:12:58 +0100

    init commit

--- src/Chapter2/AccountTest.java
+++ src/Chapter2/AccountTest.java
@@ -1,15 +1,30 @@
 package Chapter2;
 import java.util.Scanner;
 public class AccountTest {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        Account myAccount = new Account();
-        System.out.printf("Initial name is: %s%n%n" , myAccount.getName());
-        System.out.println("please enter the name: ");
-        String theName =input.nextLine();
-        myAccount.setName(theName);
-        System.out.println();
-        System.out.printf("Name in object myAccount is:%n%s%n" ,myAccount.getName());
+    public static void main(String[] args) {
+        Account account1 = new Account("Peter Parker", 50.00);
+        Account account2 = new Account("Harry Osborne", -7.53);
+        System.out.printf("%s balance:$%.2f%n",account1.getName(), account1.getBalance());
+        System.out.printf("%s balance:$%.2f%n",account2.getName(), account2.getBalance());
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter deposit amount for account1: ");
+        double depositAmount = input.nextDouble();
+        System.out.printf("%n adding %.2f to account1 balance%n%n", depositAmount);
+        account1.deposit(depositAmount);
+
+        System.out.printf("%s balance:$%.2f%n",account1.getName(), account1.getBalance());
+        System.out.printf("%s balance:$%.2f%n",account2.getName(), account2.getBalance());
+
+        System.out.print("Enter deposit amount for account2: ");
+         depositAmount = input.nextDouble();
+        System.out.printf("%n adding %.2f to account2 balance%n%n", depositAmount);
+        account2.deposit(depositAmount);
+
+        System.out.printf("%s balance:$%.2f%n",account1.getName(), account1.getBalance());
+        System.out.printf("%s balance:$%.2f%n",account2.getName(), account2.getBalance());
     }
 }
+
+
+
