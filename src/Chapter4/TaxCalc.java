86770913a847d56f513c5a93e4b9825cdfa8082f
David <chuksdavid2124@gmail.com>
2025-08-13 17:02:29 +0100

    init commit

--- src/Chapter4/TaxCalc.java
+++ src/Chapter4/TaxCalc.java
@@ -5,15 +5,18 @@ public class TaxCalc {
         int citizens = 1;
         double Tax1 = 0.15;
         double Tax2 = 0.20;
-        double totalTax = 0.00;
         Scanner input = new Scanner(System.in);
 
+
         while (citizens <= 3){
+            double totalTax = 0.00;
             System.out.println("Enter citizen name: ");
             String citizenName = input.next();
             System.out.println("Enter earnings: ");
             double earnings = input.nextDouble();
             citizens += 1;
+
+
 
             if (earnings <= 30000.00){
                 totalTax= earnings * Tax1;
@@ -21,9 +24,9 @@ public class TaxCalc {
             else{
                 totalTax += earnings * Tax2;
             }
+            System.out.printf("Your Total Tax is: %.2f%n", totalTax);
 
 
         }
-        System.out.printf("The total tax of 3 citizens is %.2f", totalTax);
     }
 }
